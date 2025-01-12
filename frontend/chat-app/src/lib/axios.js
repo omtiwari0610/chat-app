888a8f6d325b4b6270c2727a4d52eb6235d0b279
Om <omtiwari0610@gmail.com>
2025-01-12 22:02:43 +0530

    Production bug removed

--- frontend/chat-app/src/lib/axios.js
+++ frontend/chat-app/src/lib/axios.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+const isDevelopment = window.location.hostname === "localhost";
+
 export const axiosInstance = axios.create(
     {
-        baseURL : import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+        baseURL : isDevelopment ? "http://localhost:5001/api" : "/api",
         withCredentials : true,
     }
 );
